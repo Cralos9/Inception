8163b089a8ba5c521035d9136bd432b0af78b9fc
Cralos9 <carlosc.rocha77@gmail.com>
2024-09-25 13:42:08 +0100

    fix

--- Docker/srcs/requirements/mariadb/tools/init.sql
+++ Docker/srcs/requirements/mariadb/tools/init.sql
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    init.sql                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cacarval <cacarval@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/09/25 13:39:15 by cacarval          #+#    #+#              #
+#    Updated: 2024/09/25 13:39:23 by cacarval         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 CREATE DATABASE wordpress;
 CREATE USER 'wpuser'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'%' WITH GRANT OPTION;
